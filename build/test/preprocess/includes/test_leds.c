579922e2ab138204b3b2dcb0992cdf0750acc1d1
Marcos Dominguez <mrds0690@gmail.com>
2021-11-10 06:23:37 -0300

    feat: inital test

---
+++ build/test/preprocess/includes/test_leds.c
@@ -0,0 +1,2 @@
+---
+- "/var/lib/gems/2.7.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h"
---
+++ test/test_leds.c
@@ -0,0 +1,5 @@
+#include "unity.h"
+
+void test_led_apagados_al_inicio(void) {
+    TEST_FAIL_MESSAGE("Arrancamos");
+}
